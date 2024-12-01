a60c23902c277c494467aa9b004b8da32b1426c5
javier-pardo4 <javier.pardo4@alu.uclm.es>
2024-12-01 11:35:32 +0100

    prueba

---
+++ JunitExample/src/main/java/com/mslc/HelloWorld.java
@@ -0,0 +1,28 @@
+package com.mslc;
+
+public class HelloWorld {
+	 private String name = "";
+
+	   public String getName() 
+	   {
+	      return name;
+	   }
+
+	   public String getMessage() 
+	   {
+	      if (name == "") 
+	      {
+	         return "Helloo!";
+	      }
+	      else 
+	      {
+	         return "Hello " + name + "!";
+	      }
+	   }
+
+	   public void setName(String name) 
+	   {
+	      this.name = name;
+	   }
+
+}
